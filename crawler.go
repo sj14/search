891fe2215a869556d25953525b596f3e3d9d9383
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2015-04-25 22:26:44 +0200

    initial commit / before trying to switch to channels instead of slices

---
+++ crawler.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"sync"
+)
+
+var (
+	urls  []string
+	wg    sync.WaitGroup
+	mutex sync.Mutex
+	c     = make(chan string, 10) // Allocate a channel.
+
+)
+
+func main() {
+
+	//c <- "http://plus.google.com/"
+
+	wg.Add(1)
+	crawl("http://plus.google.com/")
+
+	for i := 0; i < 10; i++ {
+
+		if len(urls) > 0 {
+			wg.Add(1)
+
+			urlCrawl := urls[0]
+
+			urls = urls[1:]
+
+			go crawl(urlCrawl)
+
+		}
+	}
+	wg.Wait()
+
+}
+
+func crawl(url string) {
+	defer wg.Done()
+
+	fmt.Println("Trying to crawl: ", url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var s = string(body)
+
+	// find links
+	for cnt := strings.Count(s, "href=\""); cnt > 0; cnt-- {
+		start := strings.Index(s, "href=\"") + 6
+		if start == -1 {
+			break
+		}
+		s = s[start:]
+		end := strings.Index(s, "\"")
+		if end == -1 {
+			break
+		}
+		urlFound := s[:end]
+
+		// normalize url
+		urlFound, err = normalize(url, urlFound)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// add url to list of not yet crawled urls
+		urls = append(urls, urlFound)
+		fmt.Println("Found new url: ", urlFound)
+
+	}
+}
+
+func parseLinks() {
+
+}
+
+func normalize(urlStart, urlFound string) (string, error) {
+	// Add http if protocol isn't set
+	if len(urlFound) > 1 && urlFound[:2] == "//" {
+		urlFound = "http:" + urlFound
+	}
+	// Set start url in front if it's not set
+	if len(urlFound) > 1 && urlFound[:1] == "/" {
+		urlFound = urlStart + urlFound
+	}
+	// only add http(s) links
+	if len(urlFound) > 7 && urlFound[0:7] != "http://" {
+		if len(urlFound) > 8 && urlFound[0:8] != "https://" {
+			return "", errors.New("Protocol should be http(s)")
+		}
+	}
+	return urlFound, nil
+}
