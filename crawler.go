40783cc5cba9bd197a873136816e40891a768941
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2015-05-03 16:44:15 +0200

    stop crawling in endless loop

--- crawler.go
+++ crawler.go
@@ -3,10 +3,11 @@ package main
 import (
 	"database/sql"
 	"errors"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
 	"strings"
 	"time"
 
@@ -14,7 +15,7 @@ import (
 )
 
 var (
-	c = make(chan string, 100) // Allocate a channel.
+	c = make(chan url.URL, 100) // Allocate a channel.
 )
 
 func main() {
@@ -25,11 +26,15 @@ func main() {
 	}
 	defer db.Close()
 
-	// get first url to crawl
-	c <- popToCrawlURL(db)
+	// get first urlarg to crawl
+	//c <- popToCrawlURL(db)
 
-	for url := range c {
-		crawl(db, url)
+	startURL, _ := url.Parse("https://www.udacity.com/cs101x/index.html")
+
+	c <- *startURL
+
+	for urlarg := range c {
+		crawl(db, urlarg)
 
 		// get next url to crawl
 		c <- popToCrawlURL(db)
@@ -37,52 +42,52 @@ func main() {
 	}
 }
 
-func crawl(db *sql.DB, url string) {
+func crawl(db *sql.DB, urlarg url.URL) {
 
-	fmt.Println("Trying to crawl: ", url)
+	log.Println("Trying to crawl: ", urlarg)
 
-	var s, err = getBody(url)
+	var s, err = getBody(urlarg)
 	if err != nil {
 		return
 	}
 
-	insertBodyToURL(db, url, s)
+	insertBodyToTableURL(db, urlarg, s)
 
 	// find links
-	urlsFound := findLinks(s)
+	urlargsFound := findLinks(s)
 
-	for _, urlFound := range urlsFound {
+	for _, urlargFound := range urlargsFound {
 		// normalize url
-		urlFound, err := normalize(url, urlFound)
+
+		urlargFound, err := normalize(urlarg, urlargFound)
 		if err != nil {
-			fmt.Println(err)
+			log.Println(err)
 			return
 		}
-
+		log.Println("Found new url in body: ", urlargFound)
 		// insert into "to_crawl" table of db
-		insertToCrawlURL(db, urlFound)
-		fmt.Println("Found new url: ", urlFound)
+		insertToCrawlURL(db, urlargFound)
 	}
 }
 
-func getBody(url string) (string, error) {
-	resp, err := http.Get(url)
+func getBody(urlarg url.URL) (string, error) {
+	resp, err := http.Get(urlarg.String())
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return "", err
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return "", err
 	}
 	return string(body), nil
 }
 
-func findLinks(s string) []string {
-	var urlsFound []string
+func findLinks(s string) []url.URL {
+	var urlargsFound []url.URL
 
 	for cnt := strings.Count(s, "href=\""); cnt > 0; cnt-- {
 		start := strings.Index(s, "href=\"") + 6
@@ -94,13 +99,18 @@ func findLinks(s string) []string {
 		if end == -1 {
 			break
 		}
-		urlFound := s[:end]
-		urlsFound = append(urlsFound, urlFound)
+		urlargFound := s[:end]
+		urlFound, err := url.Parse(urlargFound)
+		if err != nil {
+			log.Println(err)
+		}
+
+		urlargsFound = append(urlargsFound, *urlFound)
 	}
-	return urlsFound
+	return urlargsFound
 }
 
-func popToCrawlURL(db *sql.DB) string {
+func popToCrawlURL(db *sql.DB) url.URL {
 
 	// Prepare statement for reading data
 	stmtOut, err := db.Prepare("SELECT id, url FROM to_crawl LIMIT 1")
@@ -110,12 +120,19 @@ func popToCrawlURL(db *sql.DB) string {
 	defer stmtOut.Close()
 
 	var id int
-	var url string // we "scan" the result in here
+	var urlarg string // we "scan" the result in here
 
 	// Query the first element found
-	err = stmtOut.QueryRow().Scan(&id, &url) // WHERE number = 13
+	err = stmtOut.QueryRow().Scan(&id, &urlarg) // WHERE number = 13
 	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
+		//panic(err.Error()) // proper error handling instead of panic in your app
+		log.Println("No more URLs to crawl. Exiting.")
+		os.Exit(0)
+	}
+
+	parsedURL, err := url.Parse(urlarg)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	// Prepare statement for deleting data
@@ -130,16 +147,31 @@ func popToCrawlURL(db *sql.DB) string {
 	if err != nil {
 		panic(err.Error()) // proper error handling instead of panic in your app
 	}
-	return url
+	return *parsedURL
 }
 
-func insertToCrawlURL(db *sql.DB, url string) {
-	// Connect to Database
-	//db, err := sql.Open("mysql", "root:@/search")
-	// if err != nil {
-	// 	panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+func insertToCrawlURL(db *sql.DB, urlarg url.URL) {
+
+	// Prepare statement for reading data
+	stmtOut, err := db.Prepare("SELECT url FROM urls WHERE url = ? LIMIT 1")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	defer stmtOut.Close()
+
+	//var dupURL string
+
+	// Query the first element found
+	err = stmtOut.QueryRow(urlarg.String()).Scan() // WHERE number = 13
+	if err != nil {
+		log.Printf("prevented adding already crawled url: %v", urlarg.String())
+		return
+	}
+
+	// if dupURL != "" {
+	// 	fmt.Println("prevented adding already crawled url")
+	// 	return
 	// }
-	// defer db.Close()
 
 	// Prepare statement for inserting data
 	stmtIns, err := db.Prepare("INSERT INTO to_crawl (url) VALUES(?)") // ? = placeholder
@@ -149,23 +181,15 @@ func insertToCrawlURL(db *sql.DB, url string) {
 	defer stmtIns.Close() // Close the statement when we leave main() / the program terminates
 
 	// Insert square numbers for 0-24 in the database
-
-	_, err = stmtIns.Exec(url) // Insert tuples (i, i^2)
+	_, err = stmtIns.Exec(urlarg.String()) // Insert tuples (i, i^2)
 	if err != nil {
 		//panic(err.Error()) // proper error handling instead of panic in your app
-		log.Print(err)
+		log.Println(err)
 	}
 
 }
 
-func insertBodyToURL(db *sql.DB, url, body string) {
-	// Connect to Database
-	//db, err := sql.Open("mysql", "root:@/search")
-	// if err != nil {
-	// 	panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
-	// }
-	// defer db.Close()
-
+func insertBodyToTableURL(db *sql.DB, urlarg url.URL, body string) {
 	// Prepare statement for inserting data
 	stmtIns, err := db.Prepare("INSERT INTO urls (url, text) VALUES(?, ?)") // ? = placeholder
 	if err != nil {
@@ -175,28 +199,46 @@ func insertBodyToURL(db *sql.DB, url, body string) {
 
 	// Insert square numbers for 0-24 in the database
 
-	_, err = stmtIns.Exec(url, body) // Insert tuples (i, i^2)
+	_, err = stmtIns.Exec(urlarg.String(), body) // Insert tuples (i, i^2)
 	if err != nil {
 		//panic(err.Error()) // proper error handling instead of panic in your app
-		log.Print(err)
+		log.Println(err)
 	}
 
 }
 
-func normalize(urlStart, urlFound string) (string, error) {
-	// Add http if protocol isn't set
-	if len(urlFound) > 1 && urlFound[:2] == "//" {
-		urlFound = "http:" + urlFound
+func normalize(urlargStart, urlargFound url.URL) (url.URL, error) {
+	// // Add http if protocol isn't set
+	// if len(urlargFound) > 1 && urlargFound[:2] == "//" {
+	// 	urlargFound = "http:" + urlargFound
+	// }
+	// // Set start urlarg in front if it's not set
+	// if len(urlargFound) > 1 && urlargFound[:1] == "/" {
+	// 	urlargFound = urlargStart + urlargFound
+	// }
+	// // only add http(s) links
+	// if len(urlargFound) > 7 && urlargFound[0:7] != "http://" {
+	// 	if len(urlargFound) > 8 && urlargFound[0:8] != "https://" {
+	// 		return "", errors.New("Protocol should be http(s)")
+	// 	}
+	// }
+
+	// Add protocol if blank
+	if urlargFound.Scheme == "" {
+		urlargFound.Scheme = urlargStart.Scheme
 	}
-	// Set start url in front if it's not set
-	if len(urlFound) > 1 && urlFound[:1] == "/" {
-		urlFound = urlStart + urlFound
+
+	// Add host if blank
+	if urlargFound.Host == "" {
+		urlargFound.Host = urlargStart.Host
 	}
+
 	// only add http(s) links
-	if len(urlFound) > 7 && urlFound[0:7] != "http://" {
-		if len(urlFound) > 8 && urlFound[0:8] != "https://" {
-			return "", errors.New("Protocol should be http(s)")
+	if urlargFound.Scheme != "http" {
+		if urlargFound.Scheme != "https" {
+			return urlargFound, errors.New("Protocol should be http(s)")
 		}
 	}
-	return urlFound, nil
+
+	return urlargFound, nil
 }
